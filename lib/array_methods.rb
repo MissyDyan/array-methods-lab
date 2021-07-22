7543f937a7e738c628b73425390a9a6fa9fed091
Diane Nguyen <diane.nguyen@gmail.com>
2021-07-22 09:32:44 -0700

    Array Methods Lab assignment completed.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
-def using_include(array, element)
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 
+def using_include(array, element)
+    element1 = "wow"
+    element2 = "Tardis"
+    array.include?(element)
 end
 
-def using_sort(array)
+using_include(array, "wow")
 
+def using_sort(array)
+    array.sort
 end
 
 def using_reverse(array)
-
+    array.reverse
 end
 
 def using_first(array)
-
+    array.first
 end
 
 def using_last(array)
-
+    array.last
 end
 
 def using_size(array)
-
+    array.size
 end
